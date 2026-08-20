85801e1bed61d35f14c6b4040216a1838e50fa0a
Huskslay <huskylover269@outlook.com>
2026-08-20 01:53:34 +0100

    Made recurse_text_to_joker local

--- src/text.lua
+++ src/text.lua
@@ -1,12 +1,12 @@
 
-function Recurse_text_to_joker(table, conversions)
+local function recurse_text_to_joker(table, conversions)
    for i, v in pairs(table) do
       if type(v) == "string" then
          for i2, v2 in pairs(conversions) do
             table[i] = table[i]:gsub(i2, v2)
          end
       elseif type(v) == "table" then
-         Recurse_text_to_joker(v, conversions)
+         recurse_text_to_joker(v, conversions)
       end
    end
 end
@@ -16,7 +16,7 @@ if BV.MOD.config.change_text then
    G.localization.descriptions.Joker.j_hanging_chad.name = "Hanging"
    G.localization.descriptions.Joker.j_wee.name = "Wee Little Joker"
 
-   Recurse_text_to_joker(
+   recurse_text_to_joker(
       G.localization,
       {
          joker="miku",
